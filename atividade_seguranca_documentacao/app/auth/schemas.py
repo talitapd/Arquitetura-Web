873ab5483940c15442550b77e0e5d44406226b10
Talita Diniz <142948881+talitapd@users.noreply.github.com>
2024-11-29 22:13:08 -0300

    Delete atividade_seguranca_documentacao directory

--- atividade_seguranca_documentacao/app/auth/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class LoginData(BaseModel):
-    email: str
-    password: str
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
--- atividade_seguranca_documentacao/app/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    SECRET_KEY: str = "my_secret_key"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
-settings = Settings()
